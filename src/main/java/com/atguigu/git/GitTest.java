081397d341d1e07e5d4d9422fae07a29e3af40e8
Lzy <lzy@qq.com>
2021-09-30 15:49:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author lzystart
+ * @create 2021-09-30 15:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
